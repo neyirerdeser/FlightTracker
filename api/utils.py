18513ec6b9829847fb9d214fee282fadd1bbee16
neyirerdeser <neyirerdeser@gmail.com>
2022-01-29 11:53:38 -0500

    utils

---
+++ api/utils.py
@@ -0,0 +1,15 @@
+def solid_example_1(*, example_param_1:str, example_param_2:int):
+	"""
+
+	:param example_param_1:
+	:param example_param_2:
+	:return:
+	"""
+
+
+
+def solid_example_2(example_param:float) -> int:
+	return 2
+
+def solid_exmaple_3(ex_par_2:float=4.3):
+	pass
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+from api import utils
+
 def main_wrapper():
 	# project start
 	print(f"function name: {main_wrapper.__name__}")
+	utils.solid_example_1(example_param_1="A", example_param_2=3)
+	utils.solid_example_2(5)
+	utils.solid_exmaple_3() # float = 4.3
+	utils.solid_exmaple_3(2.7) # float = 2.7
 # project end
 
 if __name__ == "__main__":
