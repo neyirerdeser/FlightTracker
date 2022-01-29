e79368a9d7ad0ebf94be3c4692599aaa514c5090
neyirerdeser <neyirerdeser@gmail.com>
2022-01-29 12:01:49 -0500

    utilstest

--- api/utils.py
+++ api/utils.py
@@ -11,5 +11,6 @@ def solid_example_1(*, example_param_1:str, example_param_2:int):
 def solid_example_2(example_param:float) -> int:
 	return 2
 
+#TODO:hey
 def solid_exmaple_3(ex_par_2:float=4.3):
 	pass
